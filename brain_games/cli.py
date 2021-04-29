1cc2ee25a7e55c363834edb75c186d2b5b4475e2
taiberi <harmankardon00@gmail.com>
2021-04-29 15:59:07 +0500

    make lint

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+"""CLI functions."""
+
+
 import prompt
 
+
 def welcome_user():
+    """Input your name."""
     name = prompt.string('May I have your name? ')
     print('Hello, {name}!'.format(name = name))
