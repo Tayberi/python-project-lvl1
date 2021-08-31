0a741a3d658f8d5c84918ff5c7fd6167324b6de6
tayberi <harmankardon00@gmail.com>
2021-08-31 11:00:49 +0500

    add greeting

---
+++ brain_games/cli.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+import prompt
+
+
+def welcome_user():
+    """Input your name."""
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print('Hello, {name}!'.format(name=name))
+    return name
